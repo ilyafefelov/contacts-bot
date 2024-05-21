2daa4d9f2d475ee248bd36626f4f828fcd567c4a
genkovich <genkovich@syneforge.com>
2024-05-21 23:26:13 +0200

    add notes core functional

--- src/models/note.py
+++ src/models/note.py
@@ -1,6 +1,12 @@
 class Note:
-    def __init__(self, text):
+    def __init__(self, note_id: int, title: str, text: str):
+        self.id = note_id
+        self.title = title
         self.text = text
 
+    def get_id(self):
+        return self.id
+
     def __str__(self):
-        return self.text
+        short_text = self.text[:30] + "..." if len(self.text) > 30 else self.text
+        return f"{self.id}. [{self.title}] {short_text}"
