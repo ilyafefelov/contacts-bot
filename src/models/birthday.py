32ace91b1a45f13b0684519057d82f147b502d9e
ilyafefelov <ilyafefelov@gmail.com>
2024-05-23 22:11:59 +0300

    fix: Handle future dates in Birthday field initialization

--- src/models/birthday.py
+++ src/models/birthday.py
@@ -1,10 +1,12 @@
 from src.models.field import Field
-
+from datetime import datetime, date
 
 class Birthday(Field):
     def __init__(self, value):
         try:
             self.value = datetime.strptime(value, "%d.%m.%Y").date()
-        except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+            if self.value > date.today():
+                raise ValueError("Birthday cannot be in the future.")
+        except ValueError as e:
+            raise ValueError(f"Invalid date format. Use DD.MM.YYYY. {str(e)}") from e
         super().__init__(self.value)
