3d0beacc6cfc1b43fe873b801441640ba6469ccf
ilyafefelov <ilyafefelov@gmail.com>
2024-05-24 17:25:03 +0300

    refactor: Remove unused files and dependencies + add ReadME

--- README.md
+++ README.md
@@ -1,67 +1,210 @@
-🐣🐣🐣Create New Branches from main for your Taks, then pull request them into the main branch.
+# CLI Contact and Note Manager
 
+## Overview
 
-Running the Project
-Navigate to the Project Directory:
-Ensure you are in the contact_bot directory.
+Welcome to the CLI Contact and Note Manager! This project is a command-line application designed to manage contacts and notes efficiently. Developed as part of our Master's in Computer Science program, this project aims to provide a user-friendly interface for handling various contact details and notes through simple CLI commands.
 
+## Features
 
-cd contact_bot
-Install Dependencies:
-Install the required packages from requirements.txt.
+- **Contact Management:** Add, update, delete, and search contacts with details such as phone numbers, emails, addresses, and birthdays.
+- **Note Management:** Create, edit, delete, and search notes with tags.
+- **Birthday Reminders:** View upcoming birthdays within a specified number of days.
+- **User-Friendly Commands:** Intuitive commands to perform various operations quickly and efficiently.
 
+## Download and Installation Instructions
+
+To get started with the CLI Contact and Note Manager, follow these steps:
+
+### 1. Clone the Repository
+
+Clone the repository and navigate to the project directory:
+
+```
+git clone https://github.com/ilyafefelov/contacts-bot
+cd contacts-bot
+```
+2. Install the Required Dependencies
+The dependencies are listed in the requirements.txt file. You can install them using pip:
 
 ```pip install -r requirements.txt```
 
 
+3. Run the Project
+You can run the project using the python -m command with the module path to run the main script:
 
-Run the Project:
-Use the python -m command with the module path to run the main script.
-```
-python -m src.main
-```
+```python -m path.to.main_script```
 
 
-Run Install (edit mode):
+4. Install the Project in Edit Mode
+This allows you to make changes to the project and see them reflected without having to reinstall the project. You can do this with the following command:
 
 ```pip install -e .```
 
-Run :
 
-```contact_bot```
+5. Run the Project in Edit Mode
+After installing the project in edit mode, you can run the project using the following command:
 
-
-
-
-Run Tests:
-Use unittest to run the tests.
-
-```python -m unittest discover tests```
+```python -m path.to.main_script```
 
 
 
-contact_bot/
-├── src/
-│   ├── __init__.py
-│   ├── cli/
-│   │   ├── interface.py
-│   ├── models/
-│   │   ├── address_book.py
-│   │   ├── note_book.py
-│   │   ├── record.py
-│   │   ├── note.py
-│   ├── utils/
-│   │   ├── data_handler.py
-│   │   ├── input_error.py
-│   ├── commands/
-│   │   ├── contact_commands.py
-│   │   ├── note_commands.py
-│   ├── main.py
-├── tests/
-│   ├── test_interface.py
-│   ├── test_contact_commands.py
-│   ├── test_note_commands.py
-├── .gitignore
-├── README.md
-├── requirements.txt
-├── pyproject.toml
+### CLI Commands
+Below is a detailed list of available commands and their usage:
+
+Contact Commands
+###### Add Contact
+
+```add [name] [phone] [email] [address] [birthday]```
+Add a new contact with the specified name and other details.
+
+###### Change Phone Number
+
+```change-phone [name] [old phone] [new phone]```
+Change the phone number for the specified contact.
+
+###### Show Phone Numbers
+
+```phone [name]```
+Show phone numbers for the specified contact.
+
+###### Show All Contacts
+
+```all```
+Show all contacts in the address book.
+
+###### Add Birthday
+
+```add-birthday [name] [birthday]```
+Add a birthday for the specified contact.
+
+###### Show Birthday
+
+```show-birthday [name]```
+Show the birthday for the specified contact.
+
+###### Show Upcoming Birthdays
+
+```birthdays [days]```
+Show contacts with birthdays in the next specified number of days.
+
+###### Search Contact
+
+```search [name]```
+Find a contact by name.
+
+###### Delete Contact
+
+```delete [name]```
+Delete a contact by name.
+
+###### Add Email
+
+```add-email [name] [email]```
+Add an email for the specified contact.
+
+###### Show Email
+
+```show-email [name]```
+Show the email for the specified contact.
+
+###### Change Email
+
+```change-email [name] [new email]```
+Change the email for the specified contact.
+
+###### Delete Email
+
+```delete-email [name]```
+Delete the email for the specified contact.
+
+###### Add Address
+
+```add-address [name] [address]```
+Add the address for the specified contact.
+
+###### Show Address
+
+```show-address [name]```
+Show the address for the specified contact.
+
+###### Change Address
+
+```change-address [name] [new address]```
+###### Change the address for the specified contact.
+
+Delete Address
+
+```delete-address [name]```
+Delete the address for the specified contact.
+
+#### Note Commands
+###### Add Note
+
+```add-note --title [title] --text [text]```
+Add a new note with the specified title and text.
+
+###### Get Note
+
+```get-note [ID]```
+Get a note by its ID.
+
+###### Edit Note
+
+```edit-note --id [ID] --title [title] --text [text]```
+Edit a note by its ID.
+
+###### Delete Note
+
+```delete-note [ID]```
+Delete a note by its ID.
+
+###### Add Note Tag
+
+```add-note-tag --id [ID] --tag [tag]```
+Add a tag to a note by its ID.
+
+###### Delete Note Tag
+
+```delete-note-tag --id [ID] --tag [tag]```
+Delete a tag from a note by its ID.
+
+###### Search Notes
+
+```search-notes [text]```
+Find notes by text.
+
+###### List Notes
+
+```list-notes```
+List all notes.
+
+
+
+#### General Commands
+
+###### Hello
+
+```hello```
+Get a greeting from the bot.
+
+###### Help
+
+```help```
+Display help information for all commands.
+
+###### Close or Exit
+
+```close```
+```exit```
+Close the program.
+
+
+
+### License
+This project is licensed under the MIT License. See the LICENSE file for more details.
+
+We hope you find the CLI Contact and Note Manager useful and easy to use. Your feedback is highly appreciated!
+
+Happy managing!
+
+The Project Team 4
--- src/cli/interface.py
+++ src/cli/interface.py
@@ -1,6 +1,5 @@
 from prompt_toolkit import PromptSession
 from src.cli.completer import CommandCompleter
-
 from src.commands.contact_commands import (
     add_contact,
     change_phone,
@@ -20,28 +19,32 @@ from src.commands.contact_commands import (
     add_address,
     show_address,
     change_address,
-    delete_address
+    delete_address,
 )
-
 from src.commands.note_commands import add_note, search_notes, delete_note, list_notes, edit_note, get_note_by_id, add_note_tag, delete_note_tag
 from src.utils.data_handler import save_data, load_data
-
 from typing import List, Tuple
 import re
-
 from colorama import Fore, Style, init
 
 
 init()
 
-
 AUTOSAVE_INTERVAL = 1  # Save after every command
 
 
 def parse_input(user_input: str) -> Tuple[str, List[str]]:
+    """
+    Parses the user input and returns the command and arguments.
+
+    Args:
+        user_input (str): The user input to be parsed.
+
+    Returns:
+        Tuple[str, List[str]]: A tuple containing the command and a list of arguments.
+    """
 
     if not user_input.split():
-
         return "Please enter a command:", []
 
     cmd, *args = user_input.split()
@@ -54,7 +57,46 @@ session = PromptSession(completer=CommandCompleter())
 
 
 def main():
+    """
+    The main function of the contact bot program.
 
+    This function loads the address book and notebook data from a file, prompts the user for commands,
+    and executes the corresponding actions based on the user's input.
+
+    Available commands:
+    - hello: Get a greeting from the bot.
+    - add: Add a new contact with name and other details.
+    - add-birthday: Add a birthday for the specified contact.
+    - show-birthday: Show the birthday for the specified contact.
+    - change-birthday: Change the birthday for the specified contact.
+    - birthdays: Show contacts with birthdays in the next specified number of days.
+    - change-phone: Change the phone number for the specified contact.
+    - delete-phone: Delete the phone number for the specified contact.
+    - phone: Show phone numbers for the specified contact.
+    - all: Show all contacts in the address book.
+    - search: Find a contact.
+    - delete: Delete a contact.
+    - add-note: Add a new note.
+    - get-note: Get a note by its ID.
+    - edit-note: Edit a note by its ID.
+    - add-note-tag: Add a tag to a note.
+    - delete-note-tag: Delete a tag from a note.
+    - search-notes: Find notes by text.
+    - list-notes: List all notes.
+    - delete-note: Delete a note by its ID.
+    - add-email: Add the email for the specified contact.
+    - show-email: Show the email for the specified contact.
+    - change-email: Change the email for the specified contact.
+    - delete-email: Delete the email for the specified contact.
+    - add-address: Add the address for the specified contact.
+    - show-address: Show the address for the specified contact.
+    - change-address: Change the address for the specified contact.
+    - delete-address: Delete the address for the specified contact.
+    - help: Show the list of available commands.
+
+    To exit the program, enter 'close' or 'exit'.
+
+    """
     address_book, note_book = load_data()  # Load the address book and notebook data from a file
 
     command_count = 0  # Initialize command count for autosave
@@ -62,27 +104,20 @@ def main():
     print("Welcome. I am an assistant bot!")
 
     while True:
-
         user_input = session.prompt("Enter a command: ").strip()  # Prompt the user for input
 
         if not user_input:  # Check if the user entered an empty string
-
             print("Please enter a command.")
-
             continue
 
         if user_input.lower() in ["close", "exit"]:  # Check if the user wants to exit
-
             print("Good bye!")
-
             save_data((address_book, note_book))
-
             break
 
         command, args = parse_input(user_input)
 
         def switch_commands(command):
-
             switcher = {
                 "hello": lambda: "How can I help you?",
                 "add": lambda: add_contact(args, address_book),
@@ -112,61 +147,35 @@ def main():
                 "show-address": lambda: show_address(args, address_book),
                 "change-address": lambda: change_address(args, address_book),
                 "delete-address": lambda: delete_address(args, address_book),
-                
-                "help": lambda: """ 
-
-    add [name] [phone] [email] [address] [birthday]: Add a new contact with name and other details.
-
-    change [name] [old phone] [new phone]: Change the phone number for the specified contact.
-
-    phone [name]: Show phone numbers for the specified contact.
-
-    all: Show all contacts in the address book.
-
-    add-birthday [name] [birthday]: Add a birthday for the specified contact.
-
-    show-birthday [name]: Show the birthday for the specified contact.
-
-    birthdays [days]: Show contacts with birthdays in the next specified number of days.
-
-    search [name]: Find a contact.
-
-    delete [name]: Delete a contact.
-
-    add-note --title [title] --text [text]: Add a new note.
-
-    add-email [name] [email]: Add the email for the specified contact.
-
-    show-email [name]: Show the email for the specified contact.
-
-    change-email [name] [new email]: Change the email for the specified contact.
-
-    delete-email [name]: Delete the email for the specified contact.
-
-    add-address [name] [address]: Add the address for the specified contact.
-
-    show-address [name]: Show the address for the specified contact.
-
-    change-address [name] [new address]: Change the address for the specified contact.
-
-    delete-address [name]: Delete the address for the specified contact.
-
-    list-notes: List all notes.
-
-    get-note [ID]: Get a note by its ID.
-    add-note-tag --id [ID] --tag [tag]: Add a tag to a note.
-    delete-note-tag --id [ID] --tag [tag]: Delete a tag from a note.
-
-    search-notes [text]: Find notes by text.
-
-    edit-note --id [ID] --title [title] --text [text]: Edit a note by its ID.
-
-    delete-note [ID]: Delete a note by its ID.
-
-    hello: Get a greeting from the bot.
-
-    close or exit: Close the program.
-                    """,
+                "help": lambda: """
+                    add [name] [phone] [email] [address] [birthday]: Add a new contact with name and other details.
+                    change [name] [old phone] [new phone]: Change the phone number for the specified contact.
+                    phone [name]: Show phone numbers for the specified contact.
+                    all: Show all contacts in the address book.
+                    add-birthday [name] [birthday]: Add a birthday for the specified contact.
+                    show-birthday [name]: Show the birthday for the specified contact.
+                    birthdays [days]: Show contacts with birthdays in the next specified number of days.
+                    search [name]: Find a contact.
+                    delete [name]: Delete a contact.
+                    add-note --title [title] --text [text]: Add a new note.
+                    add-email [name] [email]: Add the email for the specified contact.
+                    show-email [name]: Show the email for the specified contact.
+                    change-email [name] [new email]: Change the email for the specified contact.
+                    delete-email [name]: Delete the email for the specified contact.
+                    add-address [name] [address]: Add the address for the specified contact.
+                    show-address [name]: Show the address for the specified contact.
+                    change-address [name] [new address]: Change the address for the specified contact.
+                    delete-address [name]: Delete the address for the specified contact.
+                    list-notes: List all notes.
+                    get-note [ID]: Get a note by its ID.
+                    add-note-tag --id [ID] --tag [tag]: Add a tag to a note.
+                    delete-note-tag --id [ID] --tag [tag]: Delete a tag from a note.
+                    search-notes [text]: Find notes by text.
+                    edit-note --id [ID] --title [title] --text [text]: Edit a note by its ID.
+                    delete-note [ID]: Delete a note by its ID.
+                    hello: Get a greeting from the bot.
+                    close or exit: Close the program.
+                """,
             }
 
             result = switcher.get(
@@ -184,9 +193,7 @@ def main():
         command_count += 1
 
         if command_count >= AUTOSAVE_INTERVAL:
-
             save_data((address_book, note_book))
-
             command_count = 0
 
 
